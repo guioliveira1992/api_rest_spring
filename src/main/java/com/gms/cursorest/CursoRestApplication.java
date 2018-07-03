fa338593bc92de6777e32184050c769367282418
Guilherme <gui.oliveira1992@gmail.com>
2018-07-03 10:06:56 -0300

    meu primeiro commit

---
+++ src/main/java/com/gms/cursorest/CursoRestApplication.java
@@ -0,0 +1,12 @@
+package com.gms.cursorest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoRestApplication.class, args);
+	}
+}
